df1750de0c50d2dba880e8f135d634998770e3b3
Rohit Singh <rohit.singh@rts.com.np>
2016-09-19 16:31:58 +0545

    change gitignore

--- Objects/ringbuffer.d
+++
@@ -1,3 +0,0 @@
-.\objects\ringbuffer.o: RingBuffer.cpp
-.\objects\ringbuffer.o: RingBuffer.h
-.\objects\ringbuffer.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- Objects/system_efm32g.d
+++
@@ -1,3 +0,0 @@
-.\objects\system_efm32g.o: RTE\Device\EFM32GG980F1024\system_efm32g.c
-.\objects\system_efm32g.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\system_efm32g.o: C:\Keil_v5\ARM\PACK\Keil\EFM32Gxxx_DFP\1.2.0\Device\EFM32G\Include\em_device.h
